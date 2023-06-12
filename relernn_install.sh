de8090970b0c3c842b49304a3bfe4e3e53e3e644
Joe Manthey <jdmanthey@gmail.com>
2023-06-12 15:36:53 -0500

    Update relernn_install.sh

--- relernn_install.sh
+++ relernn_install.sh
@@ -1,3 +1,25 @@
+# interactive session on GPU partition
+interactive -p matador -c 4
+
+# set up new environment and install tensorflow (June 12, 2023)
+conda create --name recomb python=3.10
+source activate recomb
+pip install tensorflow 
+
+module load cmake/3.17.3
+
+# download and install ReLERNN (June 12, 2023)
+git clone https://github.com/kr-colab/ReLERNN.git
+cd ReLERNN
+pip install .
+
+# test installation
+cd examples
+./example_pipeline.sh
+
+
+####### old
+###########
 
 # interactive session on GPU partition
 interactive -p matador -c 4
